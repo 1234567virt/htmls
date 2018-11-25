e6de27171cef2e5f6c4a64240f4368088b1039b2
vittes <vitte@bk.ru>
2018-11-25 20:07:02 +0300

    commit 'test'

--- test.php
+++ test.php
@@ -19,15 +19,29 @@ function mathOperation($arg1, $arg2, $operation){
     switch ($operation){
         case '-':
             echo minus($arg1,$arg2);
-        break;
+             break;
         case '+':
             echo plus($arg1,$arg2);
-        break;
+            break;
         case "*":
             echo ymn($arg1,$arg2);
-        break;
+            break;
         case "/":
             echo del($arg1,$arg2);
+             break;
+    }
+}
+
+function bmr($a,$b){
+    switch(true){
+        case ($a>0 and $b>0):
+        return $a-$b;
+                break;
+    case ($a<0 and $b<0):
+        return $a*$b;
+            break;
+    case (($a<0 and $b>0) || ($b<0 and $a>0)):
+        return $a+$b;
         break;
     }
 }
