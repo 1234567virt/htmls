c0886089ffdbb97292a75a1c4bd44068535c5f04
vittes <vitte@bk.ru>
2018-11-25 20:06:26 +0300

    commit 'index'

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $year=date("Y");
         <div class='footer'> 
                 <hr>
                 <strong>
-                   Все права защищены<sup>&copy;</sup> 
+                   Все права защищены<sup>&copy;</sup> <?php echo $year; ?>
                 </strong>
             </div>   
         </div>
