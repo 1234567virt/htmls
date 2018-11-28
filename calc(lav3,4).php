83e7df3e5a832d3197dc68d296a699c9b260db32
vittes <vitte@bk.ru>
2018-11-28 13:52:46 +0300

    lavel2

--- calc(lav3,4).php
+++ calc(lav3,4).php
@@ -31,18 +31,4 @@ function mathOperation($arg1, $arg2, $operation){
              break;
     }
 }
-
-function bmr($a,$b){
-    switch(true){
-        case ($a>0 and $b>0):
-        return $a-$b;
-                break;
-    case ($a<0 and $b<0):
-        return $a*$b;
-            break;
-    case (($a<0 and $b>0) || ($b<0 and $a>0)):
-        return $a+$b;
-        break;
-    }
-}
 ?>
