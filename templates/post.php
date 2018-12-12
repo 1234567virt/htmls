b9f9511b3bc9b89c28504588576ea304b01e906c
1234567virt <44651105+1234567virt@users.noreply.github.com>
2018-12-12 16:53:51 +0300

    Update post.php

--- templates/post.php
+++ templates/post.php
@@ -1,10 +1,6 @@
 <?php 
-//require_once('/opt/lampp/htdocs/workspace.my/engine/db_connect.php');   
-//require_once('../engine/funcs.php');  
 require_once '../engine/init.php';
-
-
-  if(isset($_POST['marka'])){
+if(isset($_POST['marka'])){
   review_nout($link,$_POST['Aftor'],$_POST['Message'],$_POST['id'],$_POST['marka']);
   }
   else{
